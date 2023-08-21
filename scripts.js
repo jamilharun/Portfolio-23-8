459ae07885241c36402b2ed2935adc1efb9511d6
Jamil Harun <jamilharunl45@gmail.com>
2023-08-21 21:48:39 +0800

    js undates

--- scripts.js
+++ scripts.js
@@ -6,10 +6,12 @@ window.addEventListener('resize', () =>{console.log(window.innerWidth);});
 
 
 // --------------------------------------------------------------------------
+gsap.registerPlugin(ScrollTrigger);
+
 const header = document.querySelector(".header");
 const intro = document.querySelectorAll(".intro_L");
 const highlight = document.querySelector(".highLight")
-
+const message = document.querySelectorAll('.messageIn')
 // const icons = document.querySelectorAll(".icon-item");
 const icons = gsap.utils.toArray('.icon-item');
 
@@ -18,8 +20,8 @@ const tl2 = gsap.timeline();
 
 tl 
     .from(header, {
-        duration: 2, 
         opacity: 0, 
+        duration: 2, 
         y: '-100', 
         ease: 'power2.out'
     })
@@ -39,12 +41,39 @@ tl
 tl2 .to(icons, {
         opacity: 1,
         duration: 5,
+        // toggleActions: 'restart reverse none none',
         stagger: {
             from: 'random',
             each: 1,
             repeat: -1,
-            
         },
         ease: 'power1.inOut',
     })
 tl.add(tl2);
+
+gsap.to(message, {
+    scrollTrigger: {
+        trigger: message,
+        start: "20px 80%",
+        end : "top 100px",
+        markers: true,
+        // toggleClass: ".active",
+        // toggleActions: "restart pause reverse pause"
+    },
+    opacity: 1,
+    
+    duration: 1,
+    stagger: .5
+})
+
+
+// gsap.timeline({scrollTrigger:{
+//     trigger: message,
+//     start: 'top top',
+//     scrub: 1,
+//     opacity: 0
+// }})
+// .to(message,{
+//     height: '110%',
+//     ease: 'expo.inOut'
+// })
